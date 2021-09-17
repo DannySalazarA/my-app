a37b995c85cb623608357e790be1eeb6c79c95b2
Daniela Salazar <daniela.corpi.salazar@hotmail.com>
2021-09-17 09:02:06 -0500

    change url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:44392/',
+  baseURL: 'https://truehomewebapi.azurewebsites.net/',
   headers: {
     "Content-type": "application/json"
   }
